444fd4bec57c12c0e8b3e7ff58e2b42ebb189432
Daniel Vieira <danielvie@gmail.com>
2025-03-27 23:18:54 -0300

    add multiple targets

--- cpp/build.zig
+++ cpp/build.zig
@@ -1,19 +1,35 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
+pub fn build(b: *std.Build) !void {
+    const targets: []const std.Target.Query = &.{
+        .{ .cpu_arch = .x86, .os_tag = .windows },
+        .{ .cpu_arch = .x86_64, .os_tag = .windows },
+        // .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
+        .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
+    };
+
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addSharedLibrary(.{
-        .name = "BouncingBall",
-        .target = target,
-        .optimize = optimize,
-    });
+    for (targets) |t| {
+        const lib = b.addSharedLibrary(.{
+            .name = "BouncingBall",
+            .target = b.resolveTargetQuery(t),
+            .optimize = optimize,
+        });
 
-    lib.linkLibCpp();
-    lib.addCSourceFile(.{ .file = b.path("src/BouncingBall.cpp") });
-    lib.addCSourceFile(.{ .file = b.path("src/fmi2Functions.cpp") });
-    lib.addIncludePath(b.path("fmi2/headers/"));
+        lib.linkLibCpp();
+        lib.addCSourceFile(.{ .file = b.path("src/BouncingBall.cpp") });
+        lib.addCSourceFile(.{ .file = b.path("src/fmi2Functions.cpp") });
+        lib.addIncludePath(b.path("fmi2/headers/"));
 
-    b.installArtifact(lib);
+        const target_output = b.addInstallArtifact(lib, .{
+            .dest_dir = .{
+                .override = .{
+                    .custom = try t.zigTriple(b.allocator),
+                },
+            },
+        });
+
+        b.getInstallStep().dependOn(&target_output.step);
+    }
 }
