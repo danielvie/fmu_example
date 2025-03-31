504e3036ab648ec07b340c63a2f756724b30ca9d
Vieira, Daniel <daniel.vieira2@boeing.com>
2025-03-31 12:55:55 -0300

    moving copy logic to python

--- z_create_fmu.py
+++ z_create_fmu.py
@@ -1,6 +1,8 @@
 import zipfile
 import os
 from pathlib import Path
+import shutil
+import glob
 
 def fmu_zip(folder_name, fmu_name):
     # create a zipfile object
@@ -13,5 +15,18 @@ def fmu_zip(folder_name, fmu_name):
                 file_path = os.path.join(root, file)
                 f.write(file_path, os.path.relpath(file_path, start=folder_name))
 
-# Example usage
-fmu_zip('cpp/z_fmu_out', 'cpp_fmu_out/BouncingBall.fmu')
+def fmu_prepare(target_path: str):
+    # creating folders
+    Path(f"{target_path}/binaries/win64").mkdir(parents=True, exist_ok=True)
+    Path(f"{target_path}/binaries/linux64").mkdir(parents=True, exist_ok=True)
+
+    shutil.copy("cpp/zig-out/x86_64-windows/BouncingBall.dll", f"{target_path}/binaries/win64")
+    shutil.copy("cpp/zig-out/x86_64-linux-gnu/libBouncingBall.so", f"{target_path}/binaries/linux64/BouncingBall.so")
+    for file in glob.glob("cpp/src/*.cpp"):
+        shutil.copy(file, f"{target_path}/")
+    shutil.copy("cpp/src/modelDescription.xml", f"{target_path}/")
+
+fmu_prepare('z_fmu_out/temp')
+fmu_zip('z_fmu_out/temp', 'z_fmu_out/BouncingBall.fmu')
+
+
